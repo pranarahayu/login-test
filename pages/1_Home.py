6f7bdd55c7c2c928b79da05ca738ec114fda438d
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2024-04-24 13:44:32 +0700

    Update 1_Home.py

--- pages/1_Home.py
+++ pages/1_Home.py
@@ -1,6 +1,14 @@
 import streamlit as st
 from menu import menu
+from st_supabase_connection import SupabaseConnection
 
 menu()
 st.title("This page is available to all users")
 st.markdown(f"You are currently logged in")
+
+# Initialize connection.
+conn = st.connection("supabase",type=SupabaseConnection)
+
+# Perform query.
+rows = conn.query("*", table="mytable", ttl="10m").execute()
+st.write(rows)
