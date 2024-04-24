a571401516c8b6328cd32925e4c23d947250a03f
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2024-04-24 10:42:11 +0700

    Update menu.py

--- menu.py
+++ menu.py
@@ -9,20 +9,3 @@ def authenticated_menu():
 def unauthenticated_menu():
     # Show a navigation menu for unauthenticated users
     st.sidebar.page_link("app.py", label="Log in")
-
-
-def menu():
-    # Determine if a user is logged in or not, then show the correct
-    # navigation menu
-    if "role" not in st.session_state or st.session_state.role is None:
-      unauthenticated_menu()
-        return
-    authenticated_menu()
-
-
-def menu_with_redirect():
-    # Redirect users to the main page if not logged in, otherwise continue to
-    # render the navigation menu
-    if "role" not in st.session_state or st.session_state.role is None:
-        st.switch_page("app.py")
-    menu()
