6a7eb80563578afe500e1160e906f082af9781d9
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2024-04-24 10:28:10 +0700

    Create menu.py

---
+++ menu.py
@@ -0,0 +1,29 @@
+import streamlit as st
+
+
+def authenticated_menu():
+    # Show a navigation menu for authenticated users
+    st.sidebar.page_link("1_Home.py", label="Statistics")
+    st.sidebar.page_link("2_Season_Statistics.py", label="Statistics")
+    st.sidebar.page_link("3_Player_Detailed.py", label="Player Stats")
+
+def unauthenticated_menu():
+    # Show a navigation menu for unauthenticated users
+    st.sidebar.page_link("app.py", label="Log in")
+
+
+def menu():
+    # Determine if a user is logged in or not, then show the correct
+    # navigation menu
+    if "role" not in st.session_state or st.session_state.role is None:
+      unauthenticated_menu()
+        return
+    authenticated_menu()
+
+
+def menu_with_redirect():
+    # Redirect users to the main page if not logged in, otherwise continue to
+    # render the navigation menu
+    if "role" not in st.session_state or st.session_state.role is None:
+        st.switch_page("app.py")
+    menu()
