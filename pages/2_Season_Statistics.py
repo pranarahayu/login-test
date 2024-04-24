242dc53ee6f3b6574a8168d8f5e724601f837b67
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2024-04-24 10:45:12 +0700

    Update 2_Season_Statistics.py

--- pages/2_Season_Statistics.py
+++ pages/2_Season_Statistics.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 st.title("This page is available to all users")
-st.markdown(f"You are currently logged with the role of {st.session_state.role}.")
+st.markdown(f"You are currently logged in")
