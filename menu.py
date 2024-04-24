1ff5314957679e1add98d36e976f0413ebc4a0dc
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2024-04-24 10:54:42 +0700

    Update menu.py

--- menu.py
+++ menu.py
@@ -2,8 +2,9 @@ import streamlit as st
 
 def authenticated_menu():
     # Show a navigation menu for authenticated users
-    st.sidebar.page_link("pages/1_Home.py", label="Statistics")
+    st.sidebar.page_link("pages/1_Home.py", label="Home")
     st.sidebar.page_link("pages/2_Season_Statistics.py", label="Statistics")
+    st.sidebar.page_link("pages/3_Log_Out.py", label="Log Out")
 
 def unauthenticated_menu():
     # Show a navigation menu for unauthenticated users
