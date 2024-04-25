fc6f8d67e90255c3b6b62ab82dd12c849cab585f
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2024-04-25 10:20:18 +0700

    Update 1_Home.py

--- pages/1_Home.py
+++ pages/1_Home.py
@@ -10,11 +10,11 @@ st.markdown(f"You are currently logged in")
 # Initialize connection.
 conn = st.connection("supabase",type=SupabaseConnection)
 
+@st.cache_data(ttl=600)
 # Perform query.
 rows = conn.query("*", table="mytable", ttl="10m").execute()
 df = pd.DataFrame(rows.data)
 temp = df[['tanggal','name']]
 temp = temp.groupby(['tanggal'], as_index=False).count()
 st.line_chart(temp, x="tanggal", y="name")
-#st.write('last accessed by '+df['name'][len(df)+1]+' at '+df['tanggal'][len(df)+1]+' - '+df['waktu'][len(df)+1])
-st.write(len(df))
+st.write('last accessed by '+df['name'][len(df)-1]+' at '+df['tanggal'][len(df)-1]+' - '+df['waktu'][len(df)-1])
