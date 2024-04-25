dc1bd0db0612eb667590b022cfbebe3036475a79
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2024-04-25 10:26:12 +0700

    Update 1_Home.py

--- pages/1_Home.py
+++ pages/1_Home.py
@@ -16,4 +16,4 @@ df = pd.DataFrame(rows.data)
 temp = df[['tanggal','name']]
 temp = temp.groupby(['tanggal'], as_index=False).count()
 st.line_chart(temp, x="tanggal", y="name")
-st.write('last accessed by '+df['name'][len(df)-1]+' at '+df['tanggal'][len(df)-1]+' - '+df['waktu'][len(df)-1])
+st.write('Terakhir diakses oleh: '+df['name'][len(df)-1]+' pada '+(df['tanggal'][len(df)-1]).strftime("%d%b%Y")+' pukul '+df['waktu'][len(df)-1])
