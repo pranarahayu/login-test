d5c8053dc9c6bbfdf22e85c954e166b6c8037bca
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2024-04-29 09:11:17 +0700

    Update 2_Season_Statistics.py

--- pages/2_Season_Statistics.py
+++ pages/2_Season_Statistics.py
@@ -5,3 +5,6 @@ from streamlit_image_coordinates import streamlit_image_coordinates
 menu()
 st.title("This page is available to all users")
 st.markdown(f"You are currently logged in")
+
+value = streamlit_image_coordinates('./data/lapangkosong2.jpg', key="local",)
+st.write(value)
