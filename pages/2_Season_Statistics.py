706ed183649a720a5bfc528d5201d468b97724ee
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2024-04-29 09:47:24 +0700

    Update 2_Season_Statistics.py

--- pages/2_Season_Statistics.py
+++ pages/2_Season_Statistics.py
@@ -6,9 +6,26 @@ menu()
 st.title("This page is available to all users")
 st.markdown(f"You are currently logged in")
 
+'''
 coor = []
 value = streamlit_image_coordinates('./data/lapangkosong2.jpg', width=617.65, height=400, key="local",)
-
-coor = coor.append(value)
+coor.append(value)
 st.write(value)
 st.write(coor)
+'''
+with Image.open('./data/lapangkosong2.jpg') as img:
+    draw = ImageDraw.Draw(img)
+
+    # Draw an ellipse at each coordinate in points
+    for point in st.session_state["points"]:
+        coords = get_ellipse_coords(point)
+        draw.ellipse(coords, fill="red")
+
+    value = streamlit_image_coordinates(img, key="pil")
+
+    if value is not None:
+        point = value["x"], value["y"]
+
+        if point not in st.session_state["points"]:
+            st.session_state["points"].append(point)
+            st.experimental_rerun()
