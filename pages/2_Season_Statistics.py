369db137c5da4f5950c7ae5be01ba04e9e3ff4de
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2024-04-29 11:17:10 +0700

    Update 2_Season_Statistics.py

--- pages/2_Season_Statistics.py
+++ pages/2_Season_Statistics.py
@@ -26,7 +26,38 @@ df = pd.DataFrame(st.session_state['coor'])
 df = df.rename(columns={df.columns[0]:'X',df.columns[1]:'Y'})
 df['X'] = (df['X']*100)/xval
 df['Y'] = df['Y']/4
-st.write(df)
+
+if "data" not in st.session_state:
+    st.session_state.data = df
+
+def callback():
+    edited_rows = st.session_state["data_editor"]["edited_rows"]
+    rows_to_delete = []
+
+    for idx, value in edited_rows.items():
+        if value["x"] is True:
+            rows_to_delete.append(idx)
+
+    st.session_state["data"] = (
+        st.session_state["data"].drop(rows_to_delete, axis=0).reset_index(drop=True)
+    )
+
+
+columns = st.session_state["data"].columns
+column_config = {column: st.column_config.Column(disabled=True) for column in columns}
+
+modified_df = st.session_state["data"].copy()
+modified_df["x"] = False
+# Make Delete be the first column
+modified_df = modified_df[["x"] + modified_df.columns[:-1].tolist()]
+
+st.data_editor(
+    modified_df,
+    key="data_editor",
+    on_change=callback,
+    hide_index=True,
+    column_config=column_config,
+)
 #coor.append(value)
 #st.write(value)
 #st.write(coor)
