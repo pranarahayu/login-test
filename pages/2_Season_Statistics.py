077f2552a00acdeb39eafbe8477edb024414b0bb
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2024-04-29 10:02:15 +0700

    Update 2_Season_Statistics.py

--- pages/2_Season_Statistics.py
+++ pages/2_Season_Statistics.py
@@ -8,7 +8,16 @@ st.title("This page is available to all users")
 st.markdown(f"You are currently logged in")
 
 coor = []
+for i in st.session_state['coor']:
+  coor.append(i)
 value = streamlit_image_coordinates('./data/lapangkosong2.jpg', width=617.65, height=400, key="local",)
-coor.append(value)
+
+if value is not None:
+  point = value['x'], value['y']
+  if point not in st.session_state['coor']:
+    st.session_state['coor'].append(point)
+    st.experimental_rerun()
+
+#coor.append(value)
 #st.write(value)
-st.write(coor)
+#st.write(coor)
