d9e4cfde15b24623d056d469a9789af200559f01
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2024-09-30 15:19:34 +0700

    Update 3_Match_Center.py

--- pages/3_Match_Center.py
+++ pages/3_Match_Center.py
@@ -23,9 +23,11 @@ col1, col2, col3 = st.columns([1,2,1])
 with col1:
   st.image('./data/pnet-persib.jpg')
   st.image('./data/amap-psb.jpg')
+  st.image('./data/smap-psb.jpg')
 with col2:
   st.image('./data/stats.jpg')
   st.image('./data/momentum.jpg')
 with col3:
   st.image('./data/pnet-psbs.jpg')
   st.image('./data/amap-bik.jpg')
+  st.image('./data/smap-bik.jpg')
