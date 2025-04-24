4ce1e80150965fd105e6eba85343261931f5a770
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2025-04-24 09:51:46 +0700

    Update 1_Home.py

--- pages/1_Home.py
+++ pages/1_Home.py
@@ -7,10 +7,11 @@ from datetime import date, timedelta
 menu()
 st.title("This page is available to all users")
 st.markdown(f"You are currently logged in")
-
+'''
 conn = st.connection("postgresql", type="sql")
 
 # Perform query.
 df = conn.query('SELECT * FROM clubs;', ttl="10m")
 
 st.write(df.head(10))
+'''
